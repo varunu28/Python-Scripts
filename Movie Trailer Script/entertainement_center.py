6e02ced33553dfbc11079e7665dbc7ec64838cc6
Varun Upadhyay <varun.u28@gmail.com>
2016-01-13 20:21:52 +0530

    Working on movie trailer script

---
+++ Movie Trailer Script/entertainement_center.py
@@ -0,0 +1,5 @@
+import media
+
+toystory = media.Movie("Toy Story","A story of a boy and his toys that come to life","https://en.wikipedia.org/wiki/Toy_Story","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toystory.storyline)
